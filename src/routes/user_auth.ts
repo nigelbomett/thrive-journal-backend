f00bd6e35d69c70c5975f76ca4be7b7baf9229dc
Nigel Bomett <nigelbomett@gmail.com>
2024-07-03 22:52:46 +0300

    added login route

--- src/routes/user_auth.ts
+++ src/routes/user_auth.ts
@@ -1,7 +1,10 @@
 import express,{Request,Response} from 'express';
 import { User } from '../models/User';
 
+
 var bcrypt = require('bcryptjs');
+
+var jwt = require('jsonwebtoken');
 
 //create router object to handle requests
 const router = express.Router();
@@ -11,6 +14,7 @@ const isValidEmail = (email:string): boolean => {
     return emailRegex.test(email);
 }
 
+//add new user
 router.post('/register',async(req:Request,res:Response) => {
     try {
         const {username,email,password} = req.body;
@@ -40,5 +44,39 @@ router.post('/register',async(req:Request,res:Response) => {
         res.status(400).json({error: error.message});
     }
 });
+
+//login route
+router.post('/login',async (req:Request,res:Response) => {
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({error: 'Please provide all the required details'});
+    }
+
+    //validate email
+    if (!isValidEmail(email)) {
+        return res.status(400).json({ error: 'Invalid email format' });
+    }
+
+    //check if user is in the database 
+    const user = await User.findOne({where: {email:email}});
+
+    if (!user) {
+        return res.status(400).send('Invalid email or password');
+    }
+
+    //compare the password with the stored hash
+    const isMatch = await bcrypt.compare(password, user?.password_hash);
+
+    if(!isMatch){
+        return res.status(400).send('Invalid email or password');   
+    }
+
+    //Generate a JWT token
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {expiresIn: '12h'});
+    res.json({token});
+});
+
+
 
 export default router;
